9f0c8e355e416ffaaed0b3d836582edfee08f9d0
Donal Geraghty <DonalGeraghty@users.noreply.github.com>
2023-04-23 15:06:33 +0100

    handle extra cases

--- src/test/java/BracketsOpenClosedTest.java
+++ src/test/java/BracketsOpenClosedTest.java
@@ -43,44 +43,62 @@ public class BracketsOpenClosedTest {
         assertEquals(isValid(testString), "invalid");
     }
 
+    @Test
+    @DisplayName("Test null")
+    void sixthTest() {
+        String testString = null;
+        assertEquals(isValid(testString), "invalid");
+    }
+
+    @Test
+    @DisplayName("Test empty")
+    void seventhTest() {
+        String testString = "";
+        assertEquals(isValid(testString), "invalid");
+    }
+
+    final char openCurly = '{';
+    final char closedCurly = '}';
+    final char openRound = '(';
+    final char closedRound = ')';
+    final String valid = "valid";
+    final String invalid = "invalid";
+
     public String isValid(String s) {
 
-        final char openCurly = '{';
-        final char closedCurly = '}';
-        final char openRound = '(';
-        final char closedRound = ')';
+        if (s == null || s.isEmpty()) {
+            return invalid;
+        } else {
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < s.length(); i++) {
+                char currentChar = s.charAt(i);
 
-        Stack<Character> stack = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char currentChar = s.charAt(i);
-
-            switch (currentChar) {
-                case openCurly, openRound -> stack.add(currentChar);
-                case closedCurly -> {
-                    if (stack.isEmpty()) { // if the first is closed, invalid
-                        return "invalid";
-                    } else if (stack.peek() == openCurly) { // if the previous makes the pair, remove both
-                        stack.pop();
-                    } else {
-                        return "invalid"; // if the previous does not make the pair, invalid
+                switch (currentChar) {
+                    case openCurly, openRound -> stack.add(currentChar);
+                    case closedCurly -> {
+                        return handleClosedBracket(stack, openCurly);
                     }
-                }
-                case closedRound -> {
-                    if (stack.isEmpty()) {
-                        return "invalid";
-                    } else if (stack.peek() == openRound) {
-                        stack.pop();
-                    } else {
-                        return "invalid";
+                    case closedRound -> {
+                        return handleClosedBracket(stack, openRound);
+                    }
+                    default -> {
+                        return invalid;
                     }
                 }
             }
+            return stack.size() == 0 ? valid : invalid;
         }
-        if (stack.size() == 0) {
-            return "valid";
+
+    }
+
+    private String handleClosedBracket(Stack<Character> stack, char c) {
+        if (stack.isEmpty()) { // if the first is closed, invalid
+            return invalid;
+        } else if (stack.peek() == c) { // if the previous makes the pair, remove both
+            stack.pop();
+            return valid;
         } else {
-            return "invalid";
+            return invalid; // if the previous does not make the pair, invalid
         }
     }
 }
