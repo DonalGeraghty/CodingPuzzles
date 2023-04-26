b306aef7c787be2b47a609d317a7c578de640fe0
Donal Geraghty <DonalGeraghty@users.noreply.github.com>
2023-04-26 17:54:15 +0100

    CountLettersTest

---
+++ src/test/java/CountLettersTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CountLettersTest {
+
+
+    @Test
+    @DisplayName("Test DONALGERAGHTY")
+    void firstTest(){
+        int num = CountOccurrencesOfLetter("DONALGERAGHTY", 'D');
+        assertEquals(num, 1);
+    }
+
+
+    @Test
+    @DisplayName("Test AANBMDKKKSA")
+    void secondTest(){
+        int num = CountOccurrencesOfLetter("AANBMDKKKSA", 'A');
+        assertEquals(num, 3);
+    }
+
+    public Integer CountOccurrencesOfLetter(String str, Character character){
+        char[] charArray = str.toCharArray();
+        ArrayList<Character> list = new ArrayList<>();
+        for (char c : charArray) {
+            list.add(c);
+        }
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        for (char c:list) {
+            hashMap.putIfAbsent(c, Collections.frequency(list, c));
+        }
+        return hashMap.get(character);
+    }
+}
